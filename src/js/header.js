a386f2c42092e4ef8a7471f1165881f5e97cad48
max9911 <visti@ymail.com>
2023-11-12 17:30:41 +0200

    modal stop scroll and comment one theme-switch 2

--- src/js/header.js
+++ src/js/header.js
@@ -1,14 +1,10 @@
 const modalW = document.querySelector('.header-modal');
 const modalWinBtn = document.querySelector('.modal-open-btn');
 const allbody = document.querySelector('body');
-// console.dir(body);
-// console.log(modalWinBtn);
 
 modalWinBtn.addEventListener('click', modWin);
-body.addEventListener('wheel', stopScroll);
 
 function modWin(event) {
-  // console.log(modalW.classList);
   if (modalW.classList.contains('hidden-modal')) {
     modalW.classList.remove('hidden-modal');
     allbody.classList.add('stop-scrolling');
@@ -17,10 +13,4 @@ function modWin(event) {
     modalW.classList.add('hidden-modal');
     allbody.classList.remove('stop-scrolling');
   }
-}
-
-function stopScroll(evt) {
-  console.log(evt);
-  evt.preventDefault();
-  evt.stopPropagation();
 }
