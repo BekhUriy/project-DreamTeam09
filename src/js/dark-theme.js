5981b623e72fbbc235e8f28fecbd18ad3391fc83
max9911 <visti@ymail.com>
2023-11-13 11:54:44 +0200

    header x-close and busket icons

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -3,7 +3,7 @@ const themeSwitch = document.getElementById('themeSwitch');
 themeSwitch.addEventListener('change', changeTheme);
 
 function changeTheme() {
-    const body = document.body;
-    body.classList.toggle('light-theme');
-    body.classList.toggle('dark-theme');
+  const body = document.body;
+  body.classList.toggle('light-theme');
+  body.classList.toggle('dark-theme');
 }
--- src/js/header.js
+++ src/js/header.js
@@ -1,22 +1,22 @@
-const modalW = document.querySelector('.header-modal');
-const modalWinBtn = document.querySelector('.modal-open-btn');
-const allbody = document.querySelector('body');
-const burger = document.querySelector('.open-icon');
+const elem = {
+  modalW: document.querySelector('.header-modal'),
+  modalWinBtn: document.querySelector('.modal-open-btn'),
+  allbody: document.querySelector('body'),
+  burger: document.querySelector('.open-icon'),
+};
 
-console.dir(burger.innerHTML);
-
-modalWinBtn.addEventListener('click', modWin);
+elem.modalWinBtn.addEventListener('click', modWin);
 
 function modWin(event) {
-  if (modalW.classList.contains('hidden-modal')) {
-    modalW.classList.remove('hidden-modal');
-    allbody.classList.add('stop-scrolling');
-    burger.innerHTML = `<use href="./img/sprit.svg#x-close"/>`;
+  if (elem.modalW.classList.contains('hidden-modal')) {
+    elem.modalW.classList.remove('hidden-modal');
+    elem.allbody.classList.add('stop-scrolling');
+    elem.burger.innerHTML = `<use href="./img/sprit.svg#x-close"/>`;
 
     return;
   } else {
-    modalW.classList.add('hidden-modal');
-    allbody.classList.remove('stop-scrolling');
-    burger.innerHTML = `<use href="./img/sprit.svg#icon-burger"/>`;
+    elem.modalW.classList.add('hidden-modal');
+    elem.allbody.classList.remove('stop-scrolling');
+    elem.burger.innerHTML = `<use href="./img/sprit.svg#icon-burger"/>`;
   }
 }
