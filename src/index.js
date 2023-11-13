c8b3ad9e6b33d3c4d06bf87bd192b94ae7173271
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2023-11-13 05:44:59 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ import './js/category-list.js';
 import './js/api-requests.js';
 import './js/header.js';
 import './js/dark-theme.js';
+import './js/about-book-modal.js';
+import './js/modals.js';
---
+++ src/js/rent-block.js
@@ -0,0 +1,80 @@
+import { API_SERVICE } from './api-requests';
+const listCreate = document.querySelector('.shopping-cards');
+const listItemCreate = document.querySelector('.shopping-card');
+const shoppingStorage = document.querySelector('.shopping-storage');
+const apiShoppingList = new API_SERVICE();
+
+function createMarcup() {
+  apiShoppingList
+    .fetchBookById(bookId)
+    .then(response => {
+      return response.data
+        .map(
+          ({
+            book_image,
+            title,
+            list_name,
+            description,
+            author,
+            buy_links: [{ name, url }],
+          }) => `
+  <li class="shopping-card">
+        <div class="shopping-card-img">
+          <img src="${book_image}" alt="${title}"/>
+        </div>
+        <div class="shopping-blok">
+          <h2 class="shopping-book-title">"${title}"</h2>
+          <p class="shopping-book-category">"${list_name}"</p>
+          <p class="shopping-book-description">"${description}"</p>
+          <p class="shopping-book-autor">"${author}"</p>
+        </div>
+        <ul class="shopping-shops">
+          <li class="shopping-shop">
+            <a
+              href="${buy_links[0].url}"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Amazon-book site"
+            >
+              <img src="./img/amazon.png" class="shopping-shop-amazon" alt="${buy_links[0].name}" />
+            </a>
+          </li>
+          <li class="shopping-shop">
+            <a
+              href="${buy_links}"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Book-shop site"
+            >
+              <img src="./img/bookshop.png" class="shopping-shop-bookshop" alt="${buy_links[0].name}" />
+            </a>
+          </li>
+        </ul>
+        <button
+          type="button"
+          class="shopping-btn-delete"
+          aria-label="Delete the book from shopping list"
+        >
+          <svg class="shopping-btn-delete-icon" width="18" height="18">
+            <use href=./img/sprit.svg#icon-trash-03"></use>
+          </svg>
+        </button>
+      </li>`
+        )
+        .join('');
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+function getShoppingItems() {
+  if (localStorage.length > 0) {
+    shoppingStorage.style.display = 'none';
+    listCreate.innerHTML = createMarcup();
+  } else {
+    listCreate.style.display = 'none';
+    shoppingStorage.style.display = 'block';
+  }
+}
+getShoppingItems();
