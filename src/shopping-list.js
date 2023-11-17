1bad520b3d1935cf97af9154d7d321a254915bed
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2023-11-17 05:43:55 +0200

    fix

---
+++ src/shopping-list.js
@@ -0,0 +1,8 @@
+import './js/api-requests.js';
+import './js/dark-theme.js';
+import './js/header.js';
+// import './js/render-book.js';
+import './js/rent-block.js';
+// import './js/scrollBtn.js';
+import './js/support-slider.js';
+import './js/modal-about.js';
