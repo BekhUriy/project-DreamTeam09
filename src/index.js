2c43bfab0f9d32ca1efd647057c5633982e48cd5
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2023-11-17 03:34:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import './js/best-sellers.js';
-import './js/scrollBtn.js';
-import './js/category-list.js';
 import './js/api-requests.js';
-import './js/header.js';
+import './js/best-sellers.js';
+import './js/category-list.js';
+import './js/category-markup.js';
 import './js/dark-theme.js';
-
-import './js/rent-block.js';
+import './js/header.js';
+import './js/modal-about.js';
+import './js/render-book.js';
+import './js/scrollBtn.js';
+import './js/set-active.js';
 import './js/support-slider.js';
